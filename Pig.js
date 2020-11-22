2b2f7be840d5f0308eb50252c2accd242b81af23
Tapish <tapishthakur76@gmail.com>
2020-11-22 11:21:05 +0530

    Added score and worked after some efforts

--- Pig.js
+++ Pig.js
@@ -5,12 +5,15 @@ class Pig extends BaseClass {
     this.Visiblity = 255;
   }
 
- display(){
+ display()
+ {
    //console.log(this.body.speed);
-   if(this.body.speed < 3){
+   if(this.body.speed < 3)
+   {
     super.display();
    }
-   else{
+   else
+   {
      World.remove(world, this.body);
      push();
      this.Visiblity = this.Visiblity - 5;
@@ -18,9 +21,13 @@ class Pig extends BaseClass {
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
      pop();
    }
-   
  }
-
-
-
+   score() 
+   {
+      if(this.Visiblity < 0 && this.Visiblity >= -1000)
+   {
+     score++;
+     console.log(score);
+   }
+   }
 };
